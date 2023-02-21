f051116cc01a2f8649543a4ac52ab5f5524b1e50
nicolahf <nicolahf@stud.ntnu.no>
2023-02-21 14:00:51 +0100

    Issue-37 Implement Axios to handle HTTP requests
    Implemented Axios to handle http requests. Created booksAPI and AuthorsAPI to handle the logic.
    
    Issue #37

--- frontend/src/api/booksAPI.js
+++ frontend/src/api/booksAPI.js
@@ -1,73 +1,91 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:4000/api"; // this is the base url for the backend
+
 //Get all books
-export const getBooksAPI = () =>{
+export const getAllBooks = () => {
     // Make a GET request to retrieve all books
-    axios.get('http://localhost:4000/api/books')
-    .then(response => {
-    // Handle the response data
-        const books = response.data;
-        console.log('All books:', books);
-        return books;
-    })
-    .catch(error => {
-    // Handle any errors
-        console.error(error);
-    });
+    axios.get(`${baseUrl}/books`)
+        .then(response => {
+            // Handle the response data
+            const books = response.data;
+            console.log('All books:', books);
+            return books;
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
 };
 
 //Get a book by id
-export const getBookByIdAPI = (id) =>{
+export const getBookById = (id) => {
     // Make a GET request to retrieve a book by id
-    axios.get(`http://localhost:4000/api/books/${id}`)
-    .then(response => {
-    // Handle the response data
-        const book = response.data;
-        console.log('Book:', book);
-        return book;
-    })
-    .catch(error => {
-    // Handle any errors
-        console.error(error);
-    });
+    axios.get(`${baseUrl}/books/${id}`)
+        .then(response => {
+            // Handle the response data
+            const book = response.data;
+            console.log('Book:', book);
+            return book;
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
 }
 
 //Get a book by title
 
-export const getBookByTitleAPI = (title) =>{
+export const getBookByTitle = (title) => {
     // Make a GET request to retrieve a book by title
-    axios.get(`http://localhost:4000/api/books/title/${title}`)
-    .then(response => {
-    // Handle the response data
-        const book = response.data;
-        console.log('Book:', book);
-        return book;
-    })
-    .catch(error => {
-    // Handle any errors
-        console.error(error);
-    });
+    axios.get(`${baseUrl}/books/title/${title}`)
+        .then(response => {
+            // Handle the response data
+            const book = response.data;
+            console.log('Book:', book);
+            return book;
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
 }
 
 // create a new book
-export const createNewBookAPI = (title, author, year) =>{
+export const createNewBook = (title, author, year, genre, desc, pages) => {
     // Make a POST request to create a new book
-    axios.post('http://localhost:4000/api/books', {
+    axios.post(`${baseUrl}/books`, {
         title: title,
-        authot: author,
-        year: year
+        author: author,
+        releaseYear: year,
+        genre: genre,
+        description: desc,
+        pages: pages
     }).then(response => {
-    // Handle the response data
+        // Handle the response data
         const book = response.data;
-        console.log('Book:', book);
+        console.log('Created new book:', book);
         return book;
     })
-    .catch(error => {
-    // Handle any errors
-        console.error(error);
-    }
-    );
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        }
+        );
 }
 
+// PATCH for updating a book will come later // TODO
 
 
+// Delete a book by id
+export const deleteBookById = (id) => {
+    // DELETE request to delete a book by id
+    axios.delete(`${baseUrl}/books/${id}`)
+        .then(() => {
+            console.log(`Book with id ${id} deleted`);
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+        });
+}
